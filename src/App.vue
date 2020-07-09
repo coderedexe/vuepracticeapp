cbe5c58a65a8c1761325d5e05b46042b601fcbfc
coderedexe <iusman290@gmail.com>
2020-07-09 21:32:32 +0530

    commit1

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+    <!-- Navbar-->
+    <navbar1></navbar1>
+    <!-- Masthead-->
+    <masthead></masthead>
+    <!-- Services-->
+    <services></services>
+    <!-- Portfolio Grid-->
+    <portfolio></portfolio>
+    <!-- About-->
+    <about></about>
+    <!-- Team-->
+    <team></team>
+    <!-- Clients-->
+    <div class="py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 my-3">
+            <a href="#!">
+              <img class="img-fluid d-block mx-auto" src="assets/img/logos/envato.jpg" alt />
+            </a>
+          </div>
+          <div class="col-md-3 col-sm-6 my-3">
+            <a href="#!">
+              <img class="img-fluid d-block mx-auto" src="assets/img/logos/designmodo.jpg" alt />
+            </a>
+          </div>
+          <div class="col-md-3 col-sm-6 my-3">
+            <a href="#!">
+              <img class="img-fluid d-block mx-auto" src="assets/img/logos/themeforest.jpg" alt />
+            </a>
+          </div>
+          <div class="col-md-3 col-sm-6 my-3">
+            <a href="#!">
+              <img class="img-fluid d-block mx-auto" src="assets/img/logos/creative-market.jpg" alt />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Contact-->
+    <contact></contact>
+    <!-- Footer-->
+    <footer1></footer1>
+    <!-- Portfolio Modals-->
+    <portfoliomodals></portfoliomodals>
+  </div>
+</template>
+
+<script>
+import portfolio from "./components/portfolio";
+import navbar1 from "./components/navbar1";
+import services from "./components/services";
+import masthead from "./components/masthead";
+import about from "./components/about";
+import team from "./components/team";
+import portfoliomodals from "./components/portfoliomodals";
+import footer1 from "./components/footer1";
+import contact from "./components/contact";
+
+export default {
+  name: "App",
+  components: {
+    portfolio,
+    navbar1,
+    services,
+    masthead,
+    about,
+    team,
+    portfoliomodals,
+    footer1,
+    contact
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/portfolio.vue
@@ -0,0 +1,66 @@
+<template>
+  <section class="page-section bg-light" id="portfolio">
+    <div class="container">
+      <div class="text-center">
+        <h2 class="section-heading text-uppercase">Portfolio</h2>
+        <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-4 col-sm-6 mb-4" v-for="(obj, key) in portfolioJSON" :key="key">
+        <div class="portfolio-item">
+          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fas fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img class="img-fluid" src="assets/img/portfolio/01-thumbnail.jpg" alt />
+          </a>
+          <div class="portfolio-caption">
+            <div class="portfolio-caption-heading">{{ obj.caption }}</div>
+            <div class="portfolio-caption-subheading text-muted">{{ obj.title }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+
+<script>
+export default {
+  name: "portfolio",
+  data: () => ({
+    portfolioJSON: [
+      {
+        img: "/example/random.jpg",
+        caption: "Initialize",
+        title: "Learning"
+      },
+      {
+        img: "/example/random.jpg",
+        caption: "Initialize1",
+        title: "Learning1"
+      },
+      {
+        img: "/example/random.jpg",
+        caption: "Initialize2",
+        title: "Learning2"
+      },
+      {
+        img: "/example/random.jpg",
+        caption: "Initialize3",
+        title: "Learning3"
+      }
+    ]
+  })
+};
+</script>
+
+
+
+<style>
+</style>
